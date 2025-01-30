830d21fe25125a0b65d8093172a8c90d0fadd835
zencoderlaksh <zencoderlaksh@gmail.com>
2025-01-30 12:59:44 +0530

    ux

--- src/views/mainPage/HomePage.jsx
+++ src/views/mainPage/HomePage.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Button from "../../components/button/Button";
 
 const HomePage = () => {
   return (
-    <div className='homepage'>
-        <div className="banner-text">
-          <h2 className=' title-text-one text-white font-[40px]'>Welcome to</h2>
-          <h1 className='title-text-two text-white'><span className='text-[#FFD979] span-text'>Van Lang</span> Tour</h1>
-        </div>
-        <div className="visit-btn">
-          <Button variant="primary">Visit Page</Button>
-
-        </div>
+    <div className="homepage">
+      <div className="banner-text">
+        <h2 className=" title-text-one text-white font-[40px]">Welcome to</h2>
+        <h1 className="title-text-two text-white">
+          <span className="text-[#FFD979] span-text">Van Lang</span> Tour
+        </h1>
+      </div>
+      <div className="visit-btn">
+        <Button className="home-btn" text="View Page" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
